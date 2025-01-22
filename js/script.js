3c394f3e8b50b9b3e1ee7d3463bc8f52f8239bcb
Gian Kretzl <gian.kretzl@outlook.com>
2025-01-22 14:23:03 -0300

    fix img nav-bar, fix transition between sections, add spinner

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let spinner = document.getElementById('spinner');
+let body = document.querySelector('body');
+
+body.style.overflow = 'hidden';
+
+if (spinner) {
+    window.addEventListener('load', () => {
+        setTimeout(() => { 
+            spinner.remove();
+            body.style.overflow = ''; 
+        }, 1300); 
+    });
+}
+
+/* function toTop */
+
+function toTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Adiciona uma animação suave ao scroll
+    });
+}
+document.querySelectorAll('nav a, .footer-nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offset = 95; 
+
+        window.scrollTo({
+            top: targetElement.offsetTop - offset, 
+            behavior: 'smooth' 
+        });
+    });
+});
+
+
